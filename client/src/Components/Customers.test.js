0c315034d2d93d79833f0ed7b0e9c2169ffbe699
Connor MacPhee <cmacphee66@gmail.com>
2021-01-27 09:43:08 +0000

    Customers test file

--- client/src/Components/Customers.test.js
+++ client/src/Components/Customers.test.js
@@ -1,1 +1,46 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
 import React from 'react';
+import Customers from './Customers';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
+
+//! Test The h1 render
+
+test('text to render on screen', () => {
+  expect('CUSTOMERS').toMatch('CUSTOMERS');
+});
+
+// describe(Customers, () => {
+//   it('displays an h1 on screen', () => {
+//     render('CUSTOMERS');
+//     const text = screen.findByText(/CUSTOMERS/i);
+//     expect(text).toMatch('CUSTOMERS');
+//   });
+// });
+
+// test('should render customer text', () => {
+//   render(<Customers />)
+//   const input = screen.getByLabelText('CUSTOMERS')
+
+// describe(‘Customers’, () => {
+//   it(‘displays text “CUSTOMERS’, () => {
+//     const { getByText } = render(<Customers />)
+
+//     getByText(‘…Loading’)
+//   })
+// })
+
+// test('loads items', () => {
+//   render(<Customers />);
+//   const items = screen.findByText(/'CUSTOMERS'/i);
+//   expect(items).toMatch('CUSTOMERS');
+// });
+
+//!Test the useEffect Hook
+
+// test('set state in useEffect function' () => {
+
+// })
